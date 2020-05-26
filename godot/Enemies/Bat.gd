66fbb0646455945ae4baa8dc1e4b00007e0f1043
Mark Achée <mark@achee.com>
2020-05-25 22:35:16 -0500

    bats only chase

--- godot/Enemies/Bat.gd
+++ godot/Enemies/Bat.gd
@@ -5,55 +5,26 @@ const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 export var ACCELERATION = 300
 export var MAX_SPEED = 50
 export var FRICTION = 200
-export var WANDER_TARGET_RANGE = 4
-
-enum {
-	IDLE,
-	WANDER,
-	CHASE
-}
 
 var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
 
-var state = CHASE
-
 onready var sprite = $AnimatedSprite
 onready var stats = $Stats
-onready var playerDetectionZone = $PlayerDetectionZone
 onready var hurtbox = $Hurtbox
 onready var softCollision = $SoftCollision
-onready var wanderController = $WanderController
 onready var animationPlayer = $AnimationPlayer
 
-func _ready():
-	state = pick_random_state([IDLE, WANDER])
+var player
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
-	
-	match state:
-		IDLE:
-			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-			seek_player()
-			if wanderController.get_time_left() == 0:
-				update_wander()
-				
-		WANDER:
-			seek_player()
-			if wanderController.get_time_left() == 0:
-				update_wander()
-			accelerate_towards_point(wanderController.target_position, delta)
-			if global_position.distance_to(wanderController.target_position) <= WANDER_TARGET_RANGE:
-				update_wander()
-			
-		CHASE:
-			var player = playerDetectionZone.player
-			if player != null:
-				accelerate_towards_point(player.global_position, delta)
-			else:
-				state = IDLE
+
+	if player != null && is_instance_valid(player):
+		accelerate_towards_point(player.global_position, delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	if softCollision.is_colliding():
 		velocity += softCollision.get_push_vector() * delta * 400
@@ -63,18 +34,6 @@ func accelerate_towards_point(point, delta):
 	var direction = global_position.direction_to(point)
 	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 	sprite.flip_h = velocity.x < 0
-
-func seek_player():
-	if playerDetectionZone.can_see_player():
-		state = CHASE
-
-func update_wander():
-	state = pick_random_state([IDLE, WANDER])
-	wanderController.start_wander_timer(rand_range(1, 3))
-
-func pick_random_state(state_list):
-	state_list.shuffle()
-	return state_list.pop_front()
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
---
+++ godot/YSort.gd
@@ -0,0 +1,6 @@
+extends YSort
+
+func _ready():
+	$Bat.player = $Player
+	$Bat2.player = $Player
+	$Bat3.player = $Player
