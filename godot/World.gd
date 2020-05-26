ae33ec8a11818375d28ae9c3479c1c9c2465ee82
Mark Achée <mark@achee.com>
2020-05-25 23:01:02 -0500

    spawn infinite bats

---
+++ godot/World.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+const bat = preload("res://Enemies/Bat.tscn")
+const spawnDelay = 1
+
+onready var ysort = $YSort
+onready var player = $YSort/Player
+onready var spawners = $Spawners.get_children()
+
+var timeSinceLastBatSpawn = 0
+var nextSpawner = 0
+
+func _process(delta):
+	timeSinceLastBatSpawn += delta
+
+	if timeSinceLastBatSpawn > spawnDelay:
+		timeSinceLastBatSpawn -= spawnDelay
+
+		var newBat = bat.instance()
+		ysort.add_child(newBat)
+		newBat.global_position = spawners[nextSpawner].global_position
+		newBat.player = player
+
+		nextSpawner += 1
+		if nextSpawner >= spawners.size():
+			nextSpawner = 0
--- godot/YSort.gd
+++
@@ -1,6 +0,0 @@
-extends YSort
-
-func _ready():
-	$Bat.player = $Player
-	$Bat2.player = $Player
-	$Bat3.player = $Player
