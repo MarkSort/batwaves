836560c624001f53c5160e2e5ec7b6f905ab96a0
Mark Achée <mark@achee.com>
2020-05-25 23:42:01 -0500

    whitespace

--- godot/Player/Player.gd
+++ godot/Player/Player.gd
@@ -35,19 +35,19 @@ func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
-		
+
 		ROLL:
 			roll_state()
-		
+
 		ATTACK:
 			attack_state()
-	
+
 func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	
+
 	if input_vector != Vector2.ZERO:
 		roll_vector = input_vector
 		swordHitbox.knockback_vector = input_vector
@@ -60,12 +60,12 @@ func move_state(delta):
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
+
 	move()
-	
+
 	if Input.is_action_just_pressed("roll"):
 		state = ROLL
-	
+
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
 
@@ -94,7 +94,7 @@ func _on_Hurtbox_area_entered(area):
 	hurtbox.create_hit_effect()
 	var playerHurtSound = PlayerHurtSound.instance()
 	get_tree().current_scene.add_child(playerHurtSound)
-	
+
 func _on_Hurtbox_invincibility_started():
 	blinkAnimationPlayer.play("Start")
 
