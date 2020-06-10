e362fdd2a77ba75d10417740a04ff354fedd14b6
Mark Achée <mark@achee.com>
2020-06-09 22:19:30 -0500

    #22 on client determine newest player/bat update available before extra processing

--- godot/Networking/Client.gd
+++ godot/Networking/Client.gd
@@ -44,7 +44,12 @@ func _process(delta):
 	client.poll()
 	webRtc.poll()
 
-	if dataChannel && dataChannel.get_available_packet_count():
+	if !dataChannel: return
+
+	var newestPlayerUpdate = { "id": 0 }
+	var newestBatUpdate = { "id": 0 }
+
+	while dataChannel.get_available_packet_count():
 		var update: PoolByteArray = dataChannel.get_packet()
 		var updateBuffer = StreamPeerBuffer.new()
 		updateBuffer.set_data_array(update)
@@ -52,117 +57,23 @@ func _process(delta):
 		if updateBuffer.get_u8() == 0:
 			var playerUpdateId = updateBuffer.get_u32()
 			if playerUpdateId < lastPlayerUpdateId:
-				return
+				continue
+			if !newestPlayerUpdate.id || playerUpdateId > newestPlayerUpdate.id:
+				newestPlayerUpdate.id = playerUpdateId
+				newestPlayerUpdate.buffer = updateBuffer
 
-			lastPlayerUpdateId = playerUpdateId
-
-			var playerUpdates = {}
-			var newPlayerIds = []
-			var playerCount = (updateBuffer.get_size() - 4 - 1) / 23
-			var i = 0
-			while i < playerCount:
-				i += 1
-				var id = updateBuffer.get_u32()
-				newPlayerIds.append(id)
-
-				playerUpdates[id] = {
-					"state": updateBuffer.get_u8(),
-					"position": Vector2(
-						updateBuffer.get_float(),
-						updateBuffer.get_float()
-					),
-					"velocity": Vector2(
-						updateBuffer.get_float(),
-						updateBuffer.get_float()
-					),
-					"health": updateBuffer.get_u8(),
-					"facing": updateBuffer.get_u8(),
-					"skin": updateBuffer.get_u8()
-				}
-
-			var removePlayers = []
-			for player in world.players.get_children():
-				if playerUpdates.has(player.id):
-					newPlayerIds.erase(player.id)
-					player.state = playerUpdates[player.id].state
-					player.position = playerUpdates[player.id].position
-					player.velocity = playerUpdates[player.id].velocity
-					if playerUpdates[player.id].health < player.health:
-						player.hurt()
-						if playerUpdates[player.id].health > 0:
-							player.startInvincibility()
-					player.health = playerUpdates[player.id].health
-					if world.playerId == player.id:
-						PlayerStats.health = player.health
-
-					var facingVector = Vector2.ZERO
-					var facing = playerUpdates[player.id].facing
-					if facing >= 6:
-						facing -= 6
-						facingVector.y = 1
-					elif facing >= 3:
-						facing -= 3
-						facingVector.y = -1
-
-					if facing == 2:
-						facingVector.x = 1
-					elif facing == 1:
-						facingVector.x = -1
-
-					player.setBlendPositions(facingVector)
-					player.roll_vector = facingVector
-
-				else:
-					removePlayers.append(player)
-
-			for player in removePlayers:
-				if world.playerId == player.id:
-					PlayerStats.health = 0
-				world.players.remove_child(player)
-				player.free()
-
-			for playerId in newPlayerIds:
-				world.playerSkins[playerId] = playerUpdates[playerId].skin
-				world.addClientPlayer(playerId)
 		else:
 			var batUpdateId = updateBuffer.get_u32()
 			if batUpdateId < lastBatUpdateId:
-				return
+				continue
+			if !newestBatUpdate.id || batUpdateId > newestBatUpdate.id:
+				newestBatUpdate.id = batUpdateId
+				newestBatUpdate.buffer = updateBuffer
 
-			lastBatUpdateId = batUpdateId
-
-			var wave = updateBuffer.get_u8()
-
-			var batCount = (updateBuffer.get_size() - 4 - 1 - 1) / 9
-			var i = 0
-			var batUpdates = {}
-			while i < batCount:
-				i += 1
-				var id = updateBuffer.get_u8()
-				batUpdates[id] = Vector2(
-					updateBuffer.get_float(),
-					updateBuffer.get_float()
-				)
-
-			var playerCount = world.players.get_child_count()
-			if batCount == 0 && playerCount > 0:
-				world.status.text = "Wave %d" % [wave]
-			elif playerCount == 0:
-				world.status.text = "Game Over\non Wave %d" % [wave]
-			else:
-				world.status.text = ""
-
-			var newBats = batUpdates.duplicate()
-			for bat in world.bats.get_children():
-				if batUpdates.has(bat.id):
-					newBats.erase(bat.id)
-					bat.position.x = batUpdates[bat.id].x
-					bat.position.y = batUpdates[bat.id].y
-				else:
-					world.removeClientBat(bat)
-
-			for id in newBats:
-				world.addClientBat(id, newBats[id])
+	if newestPlayerUpdate.id:
+		doPlayerUpdate(newestPlayerUpdate.id, newestPlayerUpdate.buffer)
+	if newestBatUpdate.id:
+		doBatUpdate(newestBatUpdate.id, newestBatUpdate.buffer)
 
 	deltaSinceLastInput += delta
 	if deltaSinceLastInput > .05:
@@ -196,6 +107,114 @@ func _process(delta):
 		inputBuffer.put_u8(actions)
 
 		dataChannel.put_packet(inputBuffer.get_data_array())
+
+func doPlayerUpdate(playerUpdateId, updateBuffer):
+	lastPlayerUpdateId = playerUpdateId
+
+	var playerUpdates = {}
+	var newPlayerIds = []
+	var playerCount = (updateBuffer.get_size() - 4 - 1) / 23
+	var i = 0
+	while i < playerCount:
+		i += 1
+		var id = updateBuffer.get_u32()
+		newPlayerIds.append(id)
+
+		playerUpdates[id] = {
+			"state": updateBuffer.get_u8(),
+			"position": Vector2(
+				updateBuffer.get_float(),
+				updateBuffer.get_float()
+			),
+			"velocity": Vector2(
+				updateBuffer.get_float(),
+				updateBuffer.get_float()
+			),
+			"health": updateBuffer.get_u8(),
+			"facing": updateBuffer.get_u8(),
+			"skin": updateBuffer.get_u8()
+		}
+
+	var removePlayers = []
+	for player in world.players.get_children():
+		if playerUpdates.has(player.id):
+			newPlayerIds.erase(player.id)
+			player.state = playerUpdates[player.id].state
+			player.position = playerUpdates[player.id].position
+			player.velocity = playerUpdates[player.id].velocity
+			if playerUpdates[player.id].health < player.health:
+				player.hurt()
+				if playerUpdates[player.id].health > 0:
+					player.startInvincibility()
+			player.health = playerUpdates[player.id].health
+			if world.playerId == player.id:
+				PlayerStats.health = player.health
+
+			var facingVector = Vector2.ZERO
+			var facing = playerUpdates[player.id].facing
+			if facing >= 6:
+				facing -= 6
+				facingVector.y = 1
+			elif facing >= 3:
+				facing -= 3
+				facingVector.y = -1
+
+			if facing == 2:
+				facingVector.x = 1
+			elif facing == 1:
+				facingVector.x = -1
+
+			player.setBlendPositions(facingVector)
+			player.roll_vector = facingVector
+
+		else:
+			removePlayers.append(player)
+
+	for player in removePlayers:
+		if world.playerId == player.id:
+			PlayerStats.health = 0
+		world.players.remove_child(player)
+		player.free()
+
+	for playerId in newPlayerIds:
+		world.playerSkins[playerId] = playerUpdates[playerId].skin
+		world.addClientPlayer(playerId)
+
+func doBatUpdate(batUpdateId, updateBuffer):
+	lastBatUpdateId = batUpdateId
+
+	var wave = updateBuffer.get_u8()
+
+	var batCount = (updateBuffer.get_size() - 4 - 1 - 1) / 9
+	var i = 0
+	var batUpdates = {}
+	while i < batCount:
+		i += 1
+		var id = updateBuffer.get_u8()
+		batUpdates[id] = Vector2(
+			updateBuffer.get_float(),
+			updateBuffer.get_float()
+		)
+
+	var playerCount = world.players.get_child_count()
+	if batCount == 0 && playerCount > 0:
+		world.status.text = "Wave %d" % [wave]
+	elif playerCount == 0:
+		world.status.text = "Game Over\non Wave %d" % [wave]
+	else:
+		world.status.text = ""
+
+	var newBats = batUpdates.duplicate()
+	for bat in world.bats.get_children():
+		if batUpdates.has(bat.id):
+			newBats.erase(bat.id)
+			bat.position.x = batUpdates[bat.id].x
+			bat.position.y = batUpdates[bat.id].y
+		else:
+			world.removeClientBat(bat)
+
+	for id in newBats:
+		world.addClientBat(id, newBats[id])
 
 
 #WebSocket
